65b64af29ef77fd79805dd76849f02d5728c9ced
Priyanka <priyankaponnam123@gmail.com>
2024-07-25 15:47:59 -0400

    Add hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -3,8 +3,8 @@ from google.cloud import storage
 def create_bucket(priyanka_name):
     """Creates a new bucket."""
     storage_client = storage.Client()
-    bucket = storage_client.create_bucket(bucket_name)
+    bucket = storage_client.create_bucket(priyanka_name)
     print(f"Bucket {bucket.name} created.")
 
 if _name_ == "_main_":
-    create_bucket("your-new-bucket-name")
+    create_bucket("priyanka_name")
