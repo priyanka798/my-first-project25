45c518dda468b620b71d628f8fd91792dc2819f0
priyankaraytech <83564724+priyanka798@users.noreply.github.com>
2024-07-29 13:38:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from google.cloud import storage
 
-def create_bucket(priyanka_name):
+def create_bucket(bucket_name):
     """Creates a new bucket."""
     storage_client = storage.Client()
-    bucket = storage_client.create_bucket(priyanka_name)
+    bucket = storage_client.create_bucket(bucket_name)
     print(f"Bucket {bucket.name} created.")
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     create_bucket("bucket-using-github")
